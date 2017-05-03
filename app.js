1caec38379bfb778b0d4ddc51fcf38b6d212973b
Epulapp <joris.bouland@gmail.com>
2017-05-03 11:37:36 +0200

    add jquery + test

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-var data[];
+var data = [];
 
 function generateBoard(size){
 	for (var i = 0; i < size; i++) {
 		data[i] = i + 1;
 	}
 
-}
+};
 
 function checkValidity(){
 
-}
+};
 
 function algoRecuitSimule(){
+	console.log($("#size").val());
 
-}
+};
 
 function algoGenetique(){
 	
-}
+};
 
 function algoTaboo(){
 	
-}
+};
