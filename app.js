a7334d8568381aa2386623909640bb4b306dfb3b
TommyCabrelli <cab.tommy@gmail.com>
2017-05-03 11:29:20 +0200

    js structure

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//Project
+var data[];
+
+function generateBoard(size){
+	for (var i = 0; i < size; i++) {
+		data[i] = i + 1;
+	}
+
+}
+
+function checkValidity(){
+
+}
+
+function algoRecuitSimule(){
+
+}
+
+function algoGenetique(){
+	
+}
+
+function algoTaboo(){
+	
+}
